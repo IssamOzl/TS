03e90c14995d215340274f299426dc9041d85eed
IssamOzl <issam_ouzali@outlook.fr>
2024-07-08 22:33:48 +0100

    all

--- hello world/dist/07/FunctionRestParameter.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-// how to pass to function an udefined number of parameters => rest param
-function buildSelectReq(tableName, ...columns) {
-    const REQ = `select ${columns.toString()} from ${tableName}`;
-    return REQ;
-}
-console.log(buildSelectReq("user", "id", "name"));
-//# sourceMappingURL=FunctionRestParameter.js.map
